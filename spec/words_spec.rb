8287dbc1d217456eae8abc7af74f001f3d723f98
Bob Muller <bobmuller.f@gmail.com>
2018-11-27 16:42:09 +0100

    buffed the specs a bit

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -1,9 +1,35 @@
 require 'words'
 
 describe 'anagram(input)' do
-  it 'returns an array of grouped arrays with the anagrams' do
+  it 'should return an empty array if array is empty' do
+    expect(anagram([])).to eq([])
+  end
+
+  it 'should return an array of arrays' do
+    input = %w[listen silent bob]
+    expect(
+      anagram(input)
+      .reduce(true) do |element1, element2|
+        element1 && element2
+        .class
+        .name == 'Array'
+      end
+    ).to eq(true)
+  end
+
+  it 'should group anagrams together 1' do
     expect(anagram(%w[listen silent bob]))
       .to eq [['listen', 'silent'], ['bob']]
-    # don't %w here because it's a nested array
+  end
+
+  it 'should group anagrams together 2' do
+    input = %w[listen silent bob]
+    output = anagram(input)
+    input = output.find { |group| group.include?('listen') }
+
+    expect(input).to include('listen')
+    expect(input).to include('silent')
+
+    expect(input.size).to eq(2)
   end
 end
