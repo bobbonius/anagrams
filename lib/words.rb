aa0176408b6d03081435b9958c342881324842b2
Bob Muller <bobmuller.f@gmail.com>
2018-11-28 15:29:23 +0100

    adjustments to the assignments

--- lib/words.rb
+++ lib/words.rb
@@ -2,10 +2,44 @@
 # Finds all anagrams in this list.
 # A word consists of the same set of letters in different order
 
-words = File.read('lib/wordlist.txt').downcase.split("\n")
+words = File.read('lib/wordlist.txt')
+            .downcase
+            .split("\n")
+
+# Finds anagrams and puts them out
 
 def anagram(input)
-  input.group_by { |element| element.chars.sort }.values
+  array = input.group_by { |element| element.chars.sort }.values
+  # finding arrays with more than 1 element returns true
+  filtered = array.select { |a| a.size > 1 }
+  # puts out each array with more than 1 element
+  filtered.each { |a| print a }
 end
 
-print anagram(words).first(10)
+anagram(words)
+
+# Anagram? True or False?
+
+def anagram?(a, b)
+  a.chars.sort == b.chars.sort
+end
+
+print anagram?('listen', 'silent')
+
+# Bonus
+
+def anagram_hash(input)
+  list = {} # empty hash
+  input.each do |word| # iterates over each word in the array
+    add_word = word.chars.sort.join # sorts the anagrams
+    if list[add_word]
+      # anagrams are added to the hash
+      list[add_word] << word
+    else
+      # adds the word in a single array
+      list[add_word] = [word]
+    end
+  end
+  # list of all anagrams and words
+  list.values
+end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -19,10 +19,29 @@ describe 'anagram(input)' do
 
   it 'should group anagrams together 1' do
     expect(anagram(%w[listen silent bob]))
-      .to eq [['listen', 'silent'], ['bob']]
+      .to eq [['listen', 'silent']]
   end
 
   it 'should group anagrams together 2' do
+    input = %w[listen silent bob]
+    output = anagram(input)
+    input = output.find { |group| group.include?('listen') }
+
+    expect(input).to include('listen')
+    expect(input).to include('silent')
+
+    expect(input.size).to eq(2)
+  end
+end
+
+describe 'anagram?(a, b)' do
+  it 'should return true if an anagram is present' do
+    expect(anagram?('silent', 'listen')).to eq(true)
+  end
+end
+
+describe 'anagram_hash(input)' do
+  it 'should group anagrams together hash version' do
     input = %w[listen silent bob]
     output = anagram(input)
     input = output.find { |group| group.include?('listen') }
